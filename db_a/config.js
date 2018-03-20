05eb9bd70e0d0e3d93a3fdc3a714f88ae1779b26
easychg <156375001@qq.com>
2018-03-20 14:52:12 +0800

    ccca

---
+++ db_a/config.js
@@ -0,0 +1,20 @@
+﻿var config = {
+    "title": "",
+    "index": [1, 2, 3, 4, 5,6,7],
+    "lunbo": [1, 2, 3, 4, 5],
+    "tuijian": [1, 2, 3, 4, 5],
+    "dianji": [1, 2, 3, 4, 5],
+    "zuixin": [1, 2, 3, 4, 5],
+    "youqinglianjie": [
+        { "name": "百度", "url": "www.baidu.com" },
+        { "name": "google", "url": "www.google.com" }
+    ],
+    "biaoqian": [
+        { "name": "手机配件", "color": "#8A9B0F" },
+        { "name": "google", "color": "#036564" }
+    ],
+    "guanggao": [
+        { "name": "广告1", "img": "www.baidu.com", "descr": "描述..." },
+        { "name": "广告2", "img": "www.baidu.com", "descr": "描述..." }
+    ]
+}
